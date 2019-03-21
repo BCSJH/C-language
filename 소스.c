fd2eee4f9bd36dbeb7b91554724bfc48da910f06
BCSJH <kimbh666@naver.com>
2019-03-21 21:48:12 +0900

    소스추가

---
+++ 소스.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+	int n;
+	int a[60] = {0, };
+	int c=0;
+	printf("숫자 : ");
+	scanf("%d", &n);
+	while (1)
+	{
+		a[c] = n % 2;//나머지
+		n = n / 2;//몫
+		c++;
+		if (n ==0)
+			break;
+	}
+
+	for (int ii =c-1; ii>=0; ii--)
+	{
+	printf("%d", a[ii]);
+	}
+}
