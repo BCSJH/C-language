0a2fcf3e8b6575f4d3af532b4ef0d9421556905b
BCSJH <kimbh666@naver.com>
2019-10-09 15:36:28 +0900

    2019.10.09

---
+++ 2019년_2학기_중간고사/2019.10.09/Thread_safe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread2
+{
+    class MyData {
+        int number = 0;
+        public int Number { get { return number; } }
+        public void Increment() {
+            number++;
+        }
+    }
+    class Thread_safe
+    {
+        static void Main(string[] args)
+        {
+            MyData data = new MyData();
+            Thread t1 = new Thread(threadFunc);
+            Thread t2 = new Thread(threadFunc);
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+            Console.WriteLine("number : " + data.Number);
+        }
+        static void threadFunc(object inst)
+        {
+            MyData data = inst as MyData;
+            for (int i = 0; i < 10000; i++) {
+                data.Increment();
+            }
+
+        }
+    }
+
+}
---
+++ 2019년_2학기_중간고사/2019.10.09/ThreadingMonitor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread2
+{
+    class ThreadingMonitor
+    {
+        int number = 0;
+
+        static void Main(string[] args)
+        {
+            ThreadingMonitor pg = new ThreadingMonitor();
+            Thread t1 = new Thread(threadFunc);
+            Thread t2 = new Thread(threadFunc);
+            t1.Start(pg);
+            t2.Start(pg);
+            t1.Join();
+            t2.Join();
+            Console.WriteLine("number : " + pg.number);
+        }
+        static void threadFunc(object inst)
+        {
+            ThreadingMonitor pg = inst as ThreadingMonitor;
+            for (int i = 0; i < 10; i++)
+            {
+                pg.number = pg.number + 1;
+
+            }
+        }
+    }
+}
