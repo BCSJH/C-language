f21da1982bf4096ff071e78cc99ddc579c1df70d
BCSJH <kimbh666@naver.com>
2019-10-12 16:56:58 +0900

    2019.10.12

---
+++ 2019년_2학기_중간고사/2019.10.12/Multi_Thread.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread2
+{   class ThreadParam {
+        public int value1;
+        public int value2;
+    }
+    class Multi_Thread
+    {
+        static void Main(string[] args) {
+            Thread t = new Thread(threadFunc);
+
+            ThreadParam param = new ThreadParam();
+            param.value1 = 10;
+            param.value2 = 20;
+
+            t.Start(param);
+        }
+
+        static void threadFunc(object initialValue) {
+            ThreadParam intValue = (ThreadParam)initialValue;
+            Console.WriteLine("{0},{1}", intValue.value1, intValue.value2);
+        }
+    }
+}
---
+++ 2019년_2학기_중간고사/2019.10.12/background_Thread.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Thread2
+{
+    class background_Thread
+    {
+        static void main(string[] args)
+        {
+            Thread t1 = new Thread(threadFunc);
+            t1.IsBackground = true;
+            //t1.Isbackground는 실행 종료에 영향을 주지 않는다.
+            //t1.Join();을 넣지 않는다면 그냥 종료 됨...
+
+            t1.Start();
+            t1.Join();
+
+        }
+        static void threadFunc(object inst)
+        {
+            Console.WriteLine("60초 후에 프로그램 종료");
+            Thread.Sleep(100 * 60);
+            Console.WriteLine("종료");
+
+        }
+    }
+}
+
