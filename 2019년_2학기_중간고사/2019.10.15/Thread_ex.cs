1a5dd7563326ae6052a4858ef1e5b0a846c2bf8b
BCSJH <kimbh666@naver.com>
2019-10-15 02:22:13 +0900

    2019.10.15

---
+++ 2019년_2학기_중간고사/2019.10.15/Thread_ex.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace safe_thread
+{
+    class MyData {
+
+        int number = 0;
+       // public object _numberLock = new object();
+        public int Number { get { return number; } }
+        public void Increament() {
+            number++;
+            /*
+            lock (_numberLock) {
+                number++;
+            }
+            */
+        }
+    }
+   
+    class Thread_ex
+    {
+        static void Main(string[] arg) {
+            MyData data = new MyData();
+
+            Thread t1 = new Thread(threadFunc);
+            Thread t2 = new Thread(threadFunc);
+
+            t1.Start(data);
+            t2.Start(data);
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine(data.Number);
+        }
+        static void threadFunc(object inst) {
+            MyData data = inst as MyData;
+
+            for (int i = 0; i < 100000; i++) {
+                //data.Increament();
+                lock (data) {
+                    data.Increament();
+                }
+            }
+        }
+    }
+}
